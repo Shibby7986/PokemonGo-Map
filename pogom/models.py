19e92c706da34d4a0fd16795211f30cf2b8fef6b
Shibby7986 <dano7986@hotmail.com>
2016-08-30 21:16:51 +1000

    Added re-activation of deactivated accounts after 2 hours

--- pogom/models.py
+++ pogom/models.py
@@ -489,6 +489,7 @@ class PoGoAccount(BaseModel):
     active = BooleanField(default=True)
     in_use = BooleanField(default=False)
     session = CharField(index=True, default=generate_session())
+    last_modified = DateTimeField(default=datetime.utcnow)
 
     @staticmethod
     def get_active_unused(count, use):
@@ -538,7 +539,7 @@ def insert_accounts():
 
 def deactivate_account(faulty_account):
     log.info("Deactivating " + faulty_account)
-    query = PoGoAccount.update(active=False, in_use=False).where(PoGoAccount.username == faulty_account)
+    query = PoGoAccount.update(active=False, in_use=False, last_modified=datetime.utcnow).where(PoGoAccount.username == faulty_account)
     query.execute()
 
 
@@ -871,6 +872,13 @@ def clean_db_loop(args):
                      .delete()
                      .where((ScannedLocation.last_modified <
                             (datetime.utcnow() - timedelta(minutes=30)))))
+            query.execute()
+
+            # Reactivate account after two hour sleep
+            query = (PoGoAccount
+                     .update(active=True, in_use=False)
+                     .where((PoGoAccount.last_modified <
+                            (datetime.utcnow() - timedelta(minutes=120)))))
             query.execute()
 
             # If desired, clear old pokemon spawns

edbddecf2419a13b583910201f72db6f0913938c
Shibby7986 <dano7986@hotmail.com>
2016-09-03 03:05:32 +1000

    add last scanned and fixing up proxy stuff
    added last scanned into the db as per the speed limiter branch to fix up
    the error caused by the last commit

--- pogom/search.py
+++ pogom/search.py
@@ -36,7 +36,7 @@ from pgoapi.utilities import f2i
 from pgoapi import utilities as util
 from pgoapi.exceptions import AuthException, NotLoggedInException
 
-from .models import parse_map, Pokemon, hex_bounds, GymDetails, parse_gyms, MainWorker, WorkerStatus, PoGoAccount, deactivate_account
+from .models import parse_map, Pokemon, hex_bounds, GymDetails, parse_gyms, MainWorker, WorkerStatus, PoGoAccount, deactivate_account, update_use_account
 from .transform import generate_location_steps
 from .fakePogoApi import FakePogoApi
 from .utils import now
@@ -249,7 +249,7 @@ def search_overseer_thread(args, method, new_location_queue, pause_bit, encrypti
 
         # Set proxy to account, using round rubin
         using_proxy = ''
-        account['proxy'] = False
+        account.update({'proxy': False})
         if args.proxy:
             using_proxy = account['proxy'] = args.proxy[count % len(args.proxy)]
             if args.proxy_display.upper() != 'FULL':
@@ -568,6 +568,9 @@ def search_worker_thread(args, account, search_items_queue, pause_bit, encryptio
 
                 # Make the actual request (finally!)
                 response_dict = map_request(api, step_location, args.jitter)
+
+                # update last scan time
+                update_use_account(account['username'])
 
                 # G'damnit, nothing back. Mark it up, sleep, carry on
                 if not response_dict:
